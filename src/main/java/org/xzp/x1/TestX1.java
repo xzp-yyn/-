62f495a42a1e9b17af55619c287605801cb41deb
xuezhanpeng <3210625646@qq.com>
2022-10-15 10:33:05 +0800

    分支提交

---
+++ src/main/java/org/xzp/x1/TestX1.java
@@ -0,0 +1,12 @@
+package org.xzp.x1;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author xuezhanpeng
+ * @Date 2022/10/15 10:32
+ * @Version 1.0
+ */
+@Component
+public class TestX1 {
+}
